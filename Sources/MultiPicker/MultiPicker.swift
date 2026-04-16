3d4ed17cea5613ebf1e892bd63e37e82098e04a3
FelixSFD <felixsfd@icloud.com>
2026-04-16 19:14:54 +0200

    revert test

--- Sources/MultiPicker/MultiPicker.swift
+++ Sources/MultiPicker/MultiPicker.swift
@@ -33,7 +33,7 @@ public struct MultiPicker<Label: View, SelectionValue: Hashable, Content: View>:
                         #endif
                         ToolbarItem(placement: toolbarPlacement) {
                             label
-                                .bold()
+                                .backport.bold()
                         }
                     }
             } label: {
